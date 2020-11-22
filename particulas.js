ad6f9e8b209a8d00126991b634679cc4264e4fa7
desarrollowebtony <desarrollowebtony@gmail.com>
2020-11-21 19:01:36 -0600

    primera edicion

---
+++ particulas.js
@@ -0,0 +1,15 @@
+    // let part1 = document.getElementById('particula1');
+    // let enMovimiento;
+
+    // enMovimiento = document.getElementById('hero').addEventListener('mousemove',
+    // function(event){
+    //     let Y = event.clientY+'px';
+    //     part1.style.transform = 'translateY(Y)';
+    //     console.log('en movimiento');
+    //     console.log(event.clientX);
+    //     console.log(Y);
+    // });
+
+let part1 = document.getElementById('particula1').addEventListener('mouseover', function(){
+    console(part1);
+});
